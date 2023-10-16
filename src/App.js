005aea4e9f3ed659e2927d4427113e36cde831d1
jayabhani <abhani57@gmail.com>
2023-10-16 18:40:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect, useState } from 'react';
+// import './App.css';
 
+// function App() {
+//   let [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     console.log(count);
+//   }, [count]);
+
+//   function increment() {
+//     setCount(count+1);
+//   }
+
+//   function decrement() {
+//     setCount(count-1);
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <h1>Counter App</h1>
+//       <h2>{count}</h2>
+//       <button onClick={increment}>+</button>
+//       <button onClick={decrement}>-</button>
+//     </div>
+//   );
+// }
+
+
+import useCustomHook from "./useCustomHook";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let {count, increment, decrement} = useCustomHook();
+
+  return(
+    <>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
   );
 }
 
 export default App;
+
+// Given a string, reverse each word in the sentence 
+
+// List out the different ways an HTML element can be accessed in JavaScript code. 
+
+// What are the ways to define a variable in JavaScript? 
+
+// What is the difference between the operators ‘==‘ & ‘===‘? 
+
+// How do you create multidimensional array? 
